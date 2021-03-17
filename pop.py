f9b876b978252610e05822f2ebd3cc5ebce61c5a
aki <okonomichiyaki@gmail.com>
2021-03-17 12:07:32 -0400

    Delete pop.py

--- pop.py
+++
@@ -1,9 +0,0 @@
-from talon import ctrl, ui, Module, Context, actions, clip, app, noise
-
-ctx = Context()
-ctx.matches = r"""
-"""
-@ctx.action_class("user")
-class pop_default_user:
-    def pop():
-        actions.user.mouse_zoom()
