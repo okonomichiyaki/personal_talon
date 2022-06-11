e0bc84346a7dab9e6925329db4226bd71eab0614
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2022-06-10 20:15:59 -0400

    shadow tactics

---
+++ games/shadow_tactics.py
@@ -0,0 +1,14 @@
+# from talon import ctrl, ui, Module, Context, actions, clip, app, noise, cron
+
+# ctx = Context()
+# ctx.matches = r"""
+# app.name: Shadow Tactics.exe
+# """
+# @ctx.action_class("user")
+# class ShadowTactics:
+#     # def hiss_down():
+#     #     print("ShadowTactics.hiss_down")
+#     #     actions.key("f2")
+
+#     # def hiss_up():
+#     #     actions.key("space:up")
