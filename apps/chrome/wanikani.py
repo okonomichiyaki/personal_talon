1467fb612fec5eec933466551234a9ff2348ce04
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2024-04-07 10:26:40 -0400

    improve wanikani toggle

--- apps/chrome/wanikani.py
+++ apps/chrome/wanikani.py
@@ -1,16 +1,42 @@
-from talon import actions,ui
+from talon import actions,ui,cron
 
+watcher = None
 wanikani = False
 
-def focus_handler(window):
+def check_title():
+    title = actions.win.title()
+    return "WaniKani / Reviews" in title or "WaniKani / Recent Mistakes" in title
+
+def check_and_toggle():
     global wanikani
-    if "WaniKani / Reviews" in window.title:
-        print("WaniKani reviews: disabling speech")
+    if check_title() and not wanikani:
+        print("WaniKani: disabling speech")
         wanikani = True
         actions.speech.disable()
-    elif wanikani:
+    elif not check_title() and wanikani:
         wanikani = False
-        print("WaniKani reviews: enabling speech")
+        print("WaniKani: enabling speech")
         actions.speech.enable()
+
+def stop_watcher():
+    global watcher
+    if not watcher:
+        return
+    cron.cancel(watcher)
+    watcher = None
+
+def start_watcher():
+    global watcher
+    if watcher:
+        return
+    watcher = cron.interval("500ms", check_and_toggle)
+
+def focus_handler(window):
+    global watcher
+    check_and_toggle()
+    if "Google Chrome" in actions.win.title():
+        start_watcher()
+    else:
+        stop_watcher()
 
 ui.register("win_focus", focus_handler)
